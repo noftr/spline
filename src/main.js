f2594b4960a34cfcb3b8a285cb80cd6af1cc5877
noftr <97229373+noftr@users.noreply.github.com>
2022-07-05 23:09:51 +0300

    1

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
+let obj;
+
+
 import { Application } from "@splinetool/runtime";
+
 
 const canvas = document.getElementById("canvas3d");
 const app = new Application(canvas);
-app
-  .load("https://prod.spline.design/C8v5wKSdTmx0JFF1/scene.splinecode")
-  .then(() => {
+app.load("https://prod.spline.design/C8v5wKSdTmx0JFF1/scene.splinecode")
+.then(() => {
+    obj = app.findObjectById("908e4bcc-4410-49e2-85a6-230d2c4fcd79");
     setTimeout(animationCube, 2000);
-  });
+});
 
 function animationCube() {
-  console.log("start anim");
-  const obj = spline.findObjectById("908e4bcc-4410-49e2-85a6-230d2c4fcd79");
-  objectToAnimate.emitEvent("mouseDown");
+    obj.emitEvent("mouseDown");
+    renderAnimation();
+}
+
+function renderAnimation() {
+  console.log("начали цикл")
+	obj.rotation.x += .005;
+  window.requestAnimationFrame(renderAnimation);
 }
